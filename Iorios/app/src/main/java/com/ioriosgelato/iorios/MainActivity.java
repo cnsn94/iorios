2f26afd67eddd21c6a319f404bf087ad47b407f2
texell <texell@umich.edu>
2015-01-17 00:13:43 -0500

    Project begin

---
+++ Iorios/app/src/main/java/com/ioriosgelato/iorios/MainActivity.java
@@ -0,0 +1,43 @@
+package com.ioriosgelato.iorios;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
+// xml TextView OG stuff
+// <TextView android:text="@string/hello_world" android:layout_width="wrap_content"
+// android:layout_height="wrap_content" />
