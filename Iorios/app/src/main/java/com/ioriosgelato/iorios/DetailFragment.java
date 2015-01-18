7e56b42a57d283e406fc38dfdbfb47fe4327923b
texell <texell@umich.edu>
2015-01-18 11:35:50 -0500

    Mhacks final
    Callin it quits

--- Iorios/app/src/main/java/com/ioriosgelato/iorios/DetailFragment.java
+++ Iorios/app/src/main/java/com/ioriosgelato/iorios/DetailFragment.java
@@ -5,6 +5,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 /**
@@ -21,12 +22,17 @@ public class DetailFragment extends Activity {
        String title = intent.getStringExtra("gelato_name");
         int rightIm = intent.getIntExtra("gelato_pic", 0);
 
-        Context context = getApplicationContext();
+        TextView text = (TextView) findViewById(R.id.gelato_name);
+        text.setText(title);
+
+        ImageView imagey = (ImageView) findViewById(R.id.detail_icon);
+        imagey.setImageResource(rightIm);
+        /*Context context = getApplicationContext();
         CharSequence text = title;
         int duration = Toast.LENGTH_SHORT;
 
         Toast toast = Toast.makeText(context, text, duration);
-        toast.show();
+        toast.show();*/
 
         //ImageView imageView = (ImageView)rightIm;
     }
